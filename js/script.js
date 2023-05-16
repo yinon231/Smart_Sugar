94377a2044508861220bf46b56a9ffbae4af2bd7
guymaimon <60132246+guymaimon@users.noreply.github.com>
2023-05-16 03:27:34 +0300

    update

---
+++ js/script.js
@@ -0,0 +1,96 @@
+
+function readURL(input) {
+   
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+        reader.onload = function(e) {
+            $('#imagePreview').css('background-image', 'url('+e.target.result +')');
+            $('#imagePreview').hide();
+            $('#imagePreview').fadeIn(650);
+        }
+        reader.readAsDataURL(input.files[0]);
+    }
+}
+$(document).ready(function(){
+    $("#imageUpload").change(function() {
+        console.log(1);
+        readURL(this);
+    });
+    $("#myTab a").click(function(e){
+        e.preventDefault();
+        $(this).tab("show");
+    });
+});
+
+
+// search //
+  // Wait for the DOM to load
+  document.addEventListener("DOMContentLoaded", function() {
+
+    // Get the search input element
+    const searchInput = document.querySelector(".search");
+
+    // Add event listener to search input for keyup event
+    searchInput.addEventListener("keyup", function() {
+
+      // Get the value of the search input
+      const searchValue = searchInput.value.toLowerCase();
+
+      // Get all rows of profiles
+      const profileRows = document.querySelectorAll(".row-profile");
+
+      // Loop through each profile row
+      profileRows.forEach(function(row) {
+
+        // Get the name of the profile
+        const name = row.querySelector(".col-3").textContent.toLowerCase();
+
+        // Check if the name contains the search value
+        if (name.includes(searchValue)) {
+          // If it does, show the profile row
+          row.style.display = "flex";
+        } else {
+          // If it doesn't, hide the profile row
+          row.style.display = "none";
+        }
+      });
+    });
+
+  });
+
+
+  // active //
+
+  function toggleActive(element) {
+    // get all the nav links
+    let navLinks = $('.nav-link');
+    
+    // loop through all the nav links
+    navLinks.each(function() {
+      // check if the current nav link is the one that was clicked
+      if ($(this)[0] === element) {
+        // add the active class to the clicked element
+        $(this).addClass('active');
+        $(this).css('background-color', '#6495ED');
+      } else {
+        // remove the active class from all other nav links
+        $(this).removeClass('active');
+        $(this).css('background-color', '');
+      }
+    });
+  }
+
+  // form ensure //
+  function submitForm() {
+    const form = document.querySelector('.form-container form');
+    const errorMessage = document.querySelector('.form-container .error-message');
+  
+    if (form.checkValidity()) {
+      alert("The Profile Saved")
+      form.submit();
+    } else {
+      errorMessage.style.display = 'block';
+    }
+  }
+  
+  
